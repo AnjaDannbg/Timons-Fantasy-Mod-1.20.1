3f2838cfc7782f95049ba45e2d115191426168c4
Anja Dannenberg <anja.dannbg@gmail.com>
2024-02-21 21:38:32 +0100

    DataGen Code fixed

--- src/main/java/net/anjadannenberg/fantasymod/datagen/ModLootTableProvider.java
+++ src/main/java/net/anjadannenberg/fantasymod/datagen/ModLootTableProvider.java
@@ -3,20 +3,16 @@ package net.anjadannenberg.fantasymod.datagen;
 import net.anjadannenberg.fantasymod.datagen.loot.ModBlockLootTables;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.loot.LootTableProvider;
-import net.minecraft.world.level.storage.loot.parameters.LootContextParamSet;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 
 import java.util.List;
 import java.util.Set;
 
 public class ModLootTableProvider {
-
-    /*
     public static LootTableProvider create(PackOutput output) {
         return new LootTableProvider(output, Set.of(), List.of(
-                new LootTableProvider.SubProviderEntry(ModBlockLootTables::new, LootContextParamSet.BLOCK)
+                new LootTableProvider.SubProviderEntry(ModBlockLootTables::new, LootContextParamSets.BLOCK)
         ));
     }
 
-
-     */
 }
